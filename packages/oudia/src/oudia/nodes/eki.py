32efc31cd761969ac1343c67cb3596713e6b376e
yocjyet <git@yocjyet.dev>
2024-09-05 04:52:16 +0900

    feat: add more attribs to eki

--- packages/oudia/src/oudia/nodes/eki.py
+++ packages/oudia/src/oudia/nodes/eki.py
@@ -16,6 +16,30 @@ class Eki(TypedNode):
     ekikibo: str
     """駅規模"""
 
+    down_main: int | None = None
+    """下りメイン"""
+
+    up_main: int | None = None
+    """上りメイン"""
+
+    jikokuhyou_track_omit: bool | None = None
+    """時刻表トラックの省略"""
+
+    jikokuhyou_jikoku_display_kudari: str | None = None
+    """時刻表時刻表示（下り）"""
+
+    jikokuhyou_jikoku_display_nobori: str | None = None
+    """時刻表時刻表示（上り）"""
+
+    jikokuhyou_syubetsu_change_display_kudari: str | None = None
+    """時刻表変更表示（下り）"""
+
+    jikokuhyou_syubetsu_change_display_nobori: str | None = None
+    """時刻表変更表示（上り）"""
+
+    diagram_color_next_eki: int | None = None
+    """次駅の色"""
+
     _children: list["Node | TypedNode"] = field(default_factory=list)
 
     @property
@@ -28,6 +52,31 @@ class Eki(TypedNode):
             ekimei=node.attributes.get_required("Ekimei"),
             ekijikokukeisiki=node.attributes.get_required(key="Ekijikokukeisiki"),
             ekikibo=node.attributes.get_required("Ekikibo"),
+            down_main=int(v) if (v := node.attributes.get("DownMain")) else None,
+            up_main=int(v) if (v := node.attributes.get("UpMain")) else None,
+            jikokuhyou_track_omit=(
+                bool(v)
+                if (v := node.trailing_attributes.get("JikokuhyouTrackOmit"))
+                else None
+            ),
+            jikokuhyou_jikoku_display_kudari=node.trailing_attributes.get(
+                "JikokuhyouJikokuDisplayKudari"
+            ),
+            jikokuhyou_jikoku_display_nobori=node.trailing_attributes.get(
+                "JikokuhyouJikokuDisplayNobori"
+            ),
+            jikokuhyou_syubetsu_change_display_kudari=(
+                node.trailing_attributes.get("JikokuhyouSyubetsuChangeDisplayKudari")
+            ),
+            jikokuhyou_syubetsu_change_display_nobori=(
+                node.trailing_attributes.get("JikokuhyouSyubetsuChangeDisplayNobori")
+            ),
+            diagram_color_next_eki=(
+                int(v)
+                if (v := node.trailing_attributes.get("DiagramColorNextEki"))
+                else None
+            ),
+            _children=node.children,
         )
 
     def to_node(self) -> Node:
@@ -37,7 +86,45 @@ class Eki(TypedNode):
                 ("Ekimei", self.ekimei),
                 ("Ekijikokukeisiki", self.ekijikokukeisiki),
                 ("Ekikibo", self.ekikibo),
+                (
+                    "DownMain",
+                    str(self.down_main) if self.down_main is not None else None,
+                ),
+                ("UpMain", str(self.up_main) if self.up_main is not None else None),
             ),
             children=Children(),
-            trailing_attributes=Attributes(),
+            trailing_attributes=Attributes(
+                (
+                    "JikokuhyouTrackOmit",
+                    (
+                        ("1" if self.jikokuhyou_track_omit else "0")
+                        if self.jikokuhyou_track_omit is not None
+                        else None
+                    ),
+                ),
+                (
+                    "JikokuhyouJikokuDisplayKudari",
+                    self.jikokuhyou_jikoku_display_kudari,
+                ),
+                (
+                    "JikokuhyouJikokuDisplayNobori",
+                    self.jikokuhyou_jikoku_display_nobori,
+                ),
+                (
+                    "JikokuhyouSyubetsuChangeDisplayKudari",
+                    self.jikokuhyou_syubetsu_change_display_kudari,
+                ),
+                (
+                    "JikokuhyouSyubetsuChangeDisplayNobori",
+                    self.jikokuhyou_syubetsu_change_display_nobori,
+                ),
+                (
+                    "DiagramColorNextEki",
+                    (
+                        str(self.diagram_color_next_eki)
+                        if self.diagram_color_next_eki is not None
+                        else None
+                    ),
+                ),
+            ),
         )
