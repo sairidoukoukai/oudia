1583d9f9eb8ce93d94c91a80e1b7c9f10121c98e
yocjyet <git@yocjyet.dev>
2024-09-04 23:22:44 +0900

    feat: add more attributes to Rosen

--- packages/oudia/src/oudia/types.py
+++ packages/oudia/src/oudia/types.py
@@ -221,11 +221,32 @@ class Eki(TypedNode):
         )
 
 
+def snake_case_to_CamelCase(snake_case: str) -> str:
+    return "".join(word.title() for word in snake_case.split("_"))
+
+
 @dataclass
 class Rosen(TypedNode):
     """路線"""
 
     rosenmei: str
+    """路線名"""
+
+    kudari_dia_alias: str | None = None
+    """下りダイア別名（OuDiaSecond.1.04+）"""
+
+    nobori_dia_alias: str | None = None
+    """上りダイア別名（OuDiaSecond.1.04+）"""
+
+    kiten_jikoku: str | None = None
+    """起点時刻"""
+
+    diagram_dgr_y_zahyou_kyori_default: int | None = None
+    """ダイヤグラムDGRY座標距離デフォルト"""
+
+    comment: str | None = None
+    """コメント"""
+
     _children: list["Node | TypedNode"] = field(
         default_factory=list,
     )
@@ -236,11 +257,29 @@ class Rosen(TypedNode):
 
     @staticmethod
     def from_node(node: Node) -> "Rosen":
-        return Rosen(rosenmei=node.attributes["Rosenmei"], _children=node.children)
+        return Rosen(
+            rosenmei=node.attributes["Rosenmei"],
+            kudari_dia_alias=node.attributes.get("KudariDiaAlias"),
+            nobori_dia_alias=node.attributes.get("NoboriDiaAlias"),
+            kiten_jikoku=node.attributes.get("KitenJikoku"),
+            diagram_dgr_y_zahyou_kyori_default=(
+                int(v)
+                if (v := node.attributes.get("DiagramDgrYZahyouKyoriDefault"))
+                else None
+            ),
+            comment=node.attributes.get("Comment"),
+            _children=node.children,
+        )
 
     def to_node(self) -> Node:
         return Node(
-            type="Rosen", attributes={"Rosenmei": self.rosenmei}, children=self.children
+            type="Rosen",
+            attributes={
+                snake_case_to_CamelCase(k): v
+                for k, v in self.__dict__.items()
+                if k != "_children" and v
+            },
+            children=self.children,
         )
 
     def __eq__(self, value: object) -> bool:
