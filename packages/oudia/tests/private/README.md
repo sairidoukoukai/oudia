c122914154c370e7c7bc8548a9f23b5933c9a351
yocjyet <git@yocjyet.dev>
2024-09-05 18:38:54 +0900

    test: add private tests

---
+++ packages/oudia/tests/private/README.md
@@ -0,0 +1,1 @@
+Put your private oud files here to test
--- packages/oudia/tests/test_authenticity.py
+++ packages/oudia/tests/test_authenticity.py
@@ -1,4 +1,5 @@
 import oudia
+from pathlib import Path
 
 
 def test_oud2_import_export_empty():
@@ -11,3 +12,17 @@ def test_oud2_import_export_empty():
         f.write(oudia.dumps(dia))
 
     assert oudia.dumps(dia) == text
+
+
+def test_oud2_import_export_private():
+    for oud in Path("./tests/private").glob("*.oud*"):
+        if oud.name.endswith(".dumped.oud"):
+            continue
+
+        with open(oud, "r", encoding="utf-8-sig") as f:
+            text = f.read()
+            dia = oudia.loads(text)
+        with open(oud.with_suffix(".dumped" + oud.suffix), "w", encoding="utf-8-sig") as f:
+            f.write(oudia.dumps(dia))
+
+        assert oudia.dumps(dia) == text
