54088679936db7ed2d30fbec480ddb4a3482269c
yocjyet <git@yocjyet.dev>
2024-09-07 00:12:33 +0900

    feat: add basic operation parsing and exporting

--- packages/oudia/src/oudia/dia/operation.py
+++ packages/oudia/src/oudia/dia/operation.py
@@ -1,6 +1,9 @@
+from dataclasses import dataclass
 from enum import Enum
 
-from oudia.dia.jikoku import Jikoku
+from oudia.dia.jikoku import Hour, Jikoku, JikokuConv, Second
+
+JIKOKU_CONV = JikokuConv(True, hour=Hour.ZERO_TO_NONE, second=Second.NOT_IF_ZERO)
 
 
 class BOperation(Enum):
@@ -28,8 +31,11 @@ class BOperation(Enum):
     """運用番号変更"""
 
 
+@dataclass
 class BeforeOperation:
     """前作業"""
+
+    # TODO: Use meaningful fields instead of dummy values, make a class for each operation type
 
     operation: BOperation
 
@@ -52,6 +58,85 @@ class BeforeOperation:
 
     before_operation_list: list["BeforeOperation"]
     after_operation_list: list["AfterOperation"]
+
+    @classmethod
+    def from_str(cls, text: str) -> "BeforeOperation":
+        # TODO:
+        return cls(
+            operation=BOperation(int(text[0])),
+            bool_data_1=False,
+            bool_data_2=False,
+            int_data_1=0,
+            int_data_2=0,
+            int_data_3=0,
+            jikoku_data_1=Jikoku(None),
+            jikoku_data_2=Jikoku(None),
+            jikoku_data_3=Jikoku(None),
+            operation_number_1=[],
+            operation_number_2=[],
+            operation_number_3=[],
+            in_out_link_code="",
+            before_operation_list=[],
+            after_operation_list=[],
+        )
+
+    def __str__(self) -> str:
+        result = f"{self.operation.value}/"
+        match self.operation:
+            case BOperation.SHUNT:
+                result += str(self.int_data_1)  # shuntTrackIndex
+                result += "$"
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # hastuJikoku
+                result += "/"
+                if self.jikoku_data_2:
+                    result += JIKOKU_CONV.encode(self.jikoku_data_2)  # chakuJikoku
+                result += "$"
+                result += "1" if self.bool_data_1 else "0"  # isDisplayChakuJikoku
+
+            case BOperation.CONNECT:
+                result += "1" if self.bool_data_1 else "0"  # IsConnectToFront
+                result += "$"
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # Connect Jikoku
+
+                # TODO: Child operations here
+
+            case BOperation.RELEASE:
+                result += str(self.int_data_1)  # Release Position
+                result += "$"
+                result += str(self.int_data_2)  # Release Index Count
+                result += "/"
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # Release Jikoku
+                # TODO: Child operations here
+
+            case BOperation.OUT:
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # Out Jikoku
+                result += "$"
+                result += self.in_out_link_code  # InOutLinkCode
+                result += "/"
+                result += ";".join(self.operation_number_1)  # Operation Number Original (joined with ;)
+
+            case BOperation.OUTER:
+                result += str(self.int_data_1)  # Outer Shihatsueki Index
+                result += "$"
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # Outer Shihatsu Jikoku
+                result += "/"
+                result += JIKOKU_CONV.encode(self.jikoku_data_2)  # Chaku Jikoku
+                result += "$"
+                result += self.in_out_link_code  # InOutLinkCode
+                result += "/"
+                result += ";".join(self.operation_number_1)  # Operation Number Original (joined with ;)
+
+            case BOperation.JUNCTION:
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # Origin Jikoku
+                result += "$"
+                result += ";".join(self.operation_number_1)  # Operation Number Temp (joined with ;)
+
+            case BOperation.NUMBER_CHANGE:
+                result += ";".join(self.operation_number_1)  # Operation Number (joined with ;)
+                if not self.bool_data_1:  # Operation Number Reverse is False
+                    result += ""
+
+        return result
 
 
 class AOperation(Enum):
@@ -79,6 +164,7 @@ class AOperation(Enum):
     """運用番号変更"""
 
 
+@dataclass
 class AfterOperation:
     """後作業"""
 
@@ -101,3 +187,73 @@ class AfterOperation:
 
     before_operation_list: list["BeforeOperation"]
     after_operation_list: list["AfterOperation"]
+
+    @classmethod
+    def from_str(cls, text: str) -> "AfterOperation":
+        # TODO:
+        return cls(
+            operation=AOperation(int(text[0])),
+            bool_data_1=False,
+            bool_data_2=False,
+            int_data_1=0,
+            int_data_2=0,
+            jikoku_data_1=Jikoku(None),
+            jikoku_data_2=Jikoku(None),
+            operation_number_1=[],
+            operation_number_2=[],
+            operation_number_3=[],
+            in_out_link_code="",
+            before_operation_list=[],
+            after_operation_list=[],
+        )
+
+    def __str__(self) -> str:
+        result = f"{self.operation.value}/"
+
+        match self.operation:
+            case AOperation.SHUNT:
+                result += str(self.int_data_1)  # shuntTrackIndex
+                result += "$"
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # Operation Hatsu Jikoku
+                result += "/"
+                if self.jikoku_data_2:  # Operation Chaku Jikoku (if not null)
+                    result += JIKOKU_CONV.encode(self.jikoku_data_2)
+                result += "$"
+                result += "1" if self.bool_data_1 else "0"  # isDisplayHatsuJikoku
+
+            case AOperation.CONNECT:
+                result += "1" if self.bool_data_1 else "0"  # IsConnectToFront
+                result += "$"
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # Connect Jikoku
+
+            case AOperation.RELEASE:
+                result += str(self.int_data_1)  # Release Position
+                result += "$"
+                result += str(self.int_data_2)  # Release Index Count
+                result += "/"
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # Release Jikoku
+
+            case AOperation.IN:
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # In Jikoku
+                result += "$"
+                result += self.in_out_link_code  # InOutLinkCode
+
+            case AOperation.OUTER:
+                result += str(self.int_data_1)  # Outer Shuchakueki Index
+                result += "$"
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # Hatsu Jikoku
+                result += "/"
+                result += JIKOKU_CONV.encode(self.jikoku_data_2)  # Outer Shuchaku Jikoku
+                result += "$"
+                result += self.in_out_link_code  # InOutLinkCode
+
+            case AOperation.JUNCTION:
+                result += JIKOKU_CONV.encode(self.jikoku_data_1)  # Terminal Jikoku
+                result += "$"
+                result += str(self.int_data_1)  # Next Junction Type
+
+            case AOperation.NUMBER_CHANGE:
+                if not self.bool_data_1:  # Operation Number Reverse is False
+                    result += ";".join(self.operation_number_1)  # Operation Number (joined with ;)
+
+        return result
--- packages/oudia/src/oudia/nodes/ressya.py
+++ packages/oudia/src/oudia/nodes/ressya.py
@@ -1,7 +1,14 @@
 from dataclasses import dataclass
+from enum import Enum
 from oudia.dia.eki_jikoku import EkiJikoku
+from oudia.dia.operation import AfterOperation, BeforeOperation
 
 from .node import EntryList, Node, TypedNode
+
+
+class OperationType(Enum):
+    AFTER = "A"
+    BEFORE = "B"
 
 
 @dataclass
@@ -31,18 +38,44 @@ class Ressya(TypedNode):
 
     @classmethod
     def from_node(cls, node: Node) -> "Ressya":
+        eki_jikoku_plain = [
+            EkiJikoku.from_str(x) if x else None for x in node.entries.get_required("EkiJikoku").split(",")
+        ]
+        # add operations
+        for key, value in node.entries.properties:
+            if key.startswith("Operation"):
+                operation_type = OperationType(key[-1])
+                id = int(key[9:-1])
+
+                if id >= len(eki_jikoku_plain) or not (current_eki_jikoku := eki_jikoku_plain[id]):
+                    raise ValueError(f"Invalid operation: Operation {id} {operation_type}")
+
+                match operation_type:
+                    case OperationType.AFTER:
+                        current_eki_jikoku.after_operation_list.append(AfterOperation.from_str(value))
+                    case OperationType.BEFORE:
+                        current_eki_jikoku.before_operation_list.append(BeforeOperation.from_str(value))
+
         return cls(
             houkou=node.entries.get("Houkou"),
             syubetsu=node.entries.get_int("Syubetsu"),
             ressyabangou=node.entries.get("Ressyabangou"),
             ressyamei=node.entries.get("Ressyamei"),
-            eki_jikoku_list=list(
-                EkiJikoku.from_str(x) if x else None for x in node.entries.get_required("EkiJikoku").split(",")
-            ),
+            eki_jikoku_list=eki_jikoku_plain,
             bikou=node.entries.get("Bikou"),
         )
 
     def to_node(self) -> Node:
+        operation_entries: list[tuple[str, str]] = []
+
+        for i, eki_jikoku in enumerate(self.eki_jikoku_list):
+            if not eki_jikoku:
+                continue
+            for operation in eki_jikoku.after_operation_list:
+                operation_entries.append((f"Operation{i}A", str(operation)))
+            for operation in eki_jikoku.before_operation_list:
+                operation_entries.append((f"Operation{i}B", str(operation)))
+
         return Node(
             type="Ressya",
             entries=EntryList(
@@ -51,6 +84,7 @@ class Ressya(TypedNode):
                 ("Ressyabangou", self.ressyabangou),
                 ("Ressyamei", self.ressyamei),
                 ("EkiJikoku", ",".join(str(x) if x else "" for x in self.eki_jikoku_list)),
+                *operation_entries,
                 ("Bikou", self.bikou),
             ),
         )
