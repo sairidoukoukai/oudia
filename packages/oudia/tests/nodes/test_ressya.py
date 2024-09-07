7f61ca4dc185f9c7a76c045e1db9e67592b859a5
yocjyet <git@yocjyet.dev>
2024-09-07 13:54:13 +0900

    test: add more ressya test cases

--- packages/oudia/tests/nodes/test_ressya.py
+++ packages/oudia/tests/nodes/test_ressya.py
@@ -1,24 +1,38 @@
+import inspect
 from oudia.nodes.ressya import Ressya
 from oudia.parser import parse
 
 
+def parse_ressya(text: str) -> Ressya:
+    ressya_node = next(parse(text))
+    return Ressya.from_node(ressya_node)
+
+
 def test_ressya():
-    ressya_str = """
-Ressya.
-Houkou=Kudari
-Syubetsu=1
-Ressyabangou=5001
-EkiJikoku=,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,1;520$3,1;52240$0,1;525$0,1;528$0,1;53130$0,1;534/$2
-Operation73B=3/514$/C34;G31,0/8$515/$0
-Operation78A=5/$0
-.
-    """.strip()
+    RESSYAS = [
+        """
+            Ressya.
+            Houkou=Kudari
+            Syubetsu=1
+            Ressyabangou=5001
+            EkiJikoku=,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,1;520$3,1;52240$0,1;525$0,1;528$0,1;53130$0,1;534/$2
+            Operation73B=3/514$/C34;G31,0/8$515/$0
+            Operation78A=5/$0
+            .
+        """,
+        """
+            Ressya.
+            Houkou=Kudari
+            Syubetsu=1
+            Ressyabangou=303
+            EkiJikoku=1;750$0,2$0,1;815/815$0,1;820/820$0
+            Operation0B=5/$
+            Operation3A=5/$0
+            .
+        """,
+    ]
 
-    ressya_node = list(parse(ressya_str))[0]
-
-    print(ressya_node)
-
-    ressya = Ressya.from_node(ressya_node)
-    print(ressya)
-
-    assert ressya_str == str(ressya)
+    for ressya_str in RESSYAS:
+        ressya_str = inspect.cleandoc(ressya_str)
+        ressya = parse_ressya(ressya_str)
+        assert ressya_str == str(ressya)
