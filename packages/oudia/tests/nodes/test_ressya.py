e8fbcef56a284583bf2419ffb4534e839edd066a
yocjyet <git@yocjyet.dev>
2024-09-08 02:57:43 +0900

    test: add test for invalid ressya operations

--- packages/oudia/tests/nodes/test_ressya.py
+++ packages/oudia/tests/nodes/test_ressya.py
@@ -1,6 +1,7 @@
 import inspect
 from oudia.nodes.ressya import Ressya
 from oudia.parser import parse
+import pytest
 
 
 def parse_ressya(text: str) -> Ressya:
@@ -37,3 +38,33 @@ def test_ressya():
         ressya_str = inspect.cleandoc(ressya_str)
         ressya = parse_ressya(ressya_str)
         assert ressya_str == str(ressya)
+
+
+def test_ressya_invalid():
+    RESSYAS = [
+        """
+            Ressya.
+            Houkou=Kudari
+            Syubetsu=1
+            Ressyabangou=303
+            EkiJikoku=1;750$0,2$0,1;815/815$0,1;820/820$0
+            Operation15B=5/$
+            Operation3A=5/$0
+            .
+        """,
+        """
+            Ressya.
+            Houkou=Kudari
+            Syubetsu=1
+            Ressyabangou=303
+            EkiJikoku=1;750$0,2$0,1;815/815$0,1;820/820$0
+            Operation3B=5/$
+            Operation15A=5/$0
+            .
+        """,
+    ]
+
+    for ressya_str in RESSYAS:
+        ressya_str = inspect.cleandoc(ressya_str)
+        with pytest.raises(ValueError):
+            ressya = parse_ressya(ressya_str)
